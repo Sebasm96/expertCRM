0d9896af810534ec94770910319161f0fa3c535b
Sebas <sebastian.maria.mog@gmail.com>
2023-09-03 02:14:44 -0500

    Changed few things. Working on Login, logout and sign up features.

--- website/views.py
+++ website/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
-# Create your views here.
+
 def home(request):
     return render(request, 'home.html', {})
+
+def log_in(request):
+    pass
+
+def log_out(request):
+    pass
+
